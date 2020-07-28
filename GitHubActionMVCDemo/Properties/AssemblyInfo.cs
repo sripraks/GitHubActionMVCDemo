a821355fb8b3912784a9b4eb223973bc30083c7e
Prakash Srinivasan <sripraks@live.com>
2020-07-28 20:32:42 +0530

    git version

--- GitHubActionMVCDemo/Properties/AssemblyInfo.cs
+++ GitHubActionMVCDemo/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("20.8.0.0")]
-[assembly: AssemblyFileVersion("20.8.0.0")]
-[assembly: AssemblyInformationalVersion("20.8.0+6.Branch.master.Sha.f9a32c112ffc9b3a9eee07db978c360fcc753565")]
+//[assembly: AssemblyVersion("20.8.0.0")]
+//[assembly: AssemblyFileVersion("20.8.0.0")]
+//[assembly: AssemblyInformationalVersion("20.8.0+6.Branch.master.Sha.f9a32c112ffc9b3a9eee07db978c360fcc753565")]
